93480681c8f00ad59e8d673c2dcaaf38108b7cee
Scott Coggeshall <sscogges@uw.edu>
2019-07-05 01:26:29 -0700

    add categorical variables to test_df

--- data-raw/testdat.R
+++ data-raw/testdat.R
@@ -15,36 +15,11 @@ test_df$x2 <- rexp(nrow(test_df))
 
 test_df$x3 <- runif(nrow(test_df))
 
+test_df$x4 <- factor(sample(c("none", "low", "medium", "high"), nrow(test_df), replace = T, prob = c(.1, .3, .4, .2)))
 
-test_tables <- survey_table(test_df, vars = c("x1", "x2"), stat_func = compute_stats, condvar_wide = "time", condvar_long = "tx")
+test_df$x5 <- factor(sample(c("white", "black", "other"), nrow(test_df), replace = T, prob = c(.7, .2, .1)))
 
-make_table(tables = test_tables, condvar_wide_labels = c("Time 1", "Time 2"), condvar_long_labels = c("No Tx", "Tx"),
-           stat_labels = c("Mean", "SD", "N"), var_labels = c("X1", "X2"))
-
-
-lapply(unique(test_df$time), function(x){
-
-  temp_list <- lapply(unique(test_df$tx[test_df$time == x]), function(y){
-
-    dat <- as.data.frame(t(sapply(test_df[test_df$time == x & test_df$tx == y, c("x1", "x2", "x3")], function(z) c(mean(z), sd(z)))))
-
-    dat$group <- y
-    dat$name <- c("x1", "x2", "x3")
-
-
-    dat
-  })
-
-  temp_df <- Reduce(rbind, temp_list)
-
-  temp_df <- temp_df[order(temp_df$name, temp_df$group), ]
-
-  temp_df[, -ncol(temp_df)]
-
-
-})
-
-names(test_df)[grep("x1|2|3", names(test_df))] <- c("bp", "height", "score")
+names(test_df)[grep("x1|2|3|4|5", names(test_df))] <- c("bp", "height", "score", "severity", "race")
 
 write.csv(test_df, "data-raw/test_df.csv")
 
