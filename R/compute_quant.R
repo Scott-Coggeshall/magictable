25214c273157aa08c465a6bf2d9f46239b5a7a79
Scott Coggeshall <sscogges@uw.edu>
2019-07-30 11:12:04 -0700

    rework compute_stats; add compute_cat

--- R/compute_quant.R
+++ R/compute_quant.R
@@ -10,37 +10,71 @@
 #'
 #' @export
 #' @return a numeric vector containing the computed statistics.
-compute_stats <- function(x, labels = NULL){
+compute_quant <- function(x, digits = 3, labels = NULL, ...){
 
-  output <- c( n = sum(!is.na(x)), mean = mean(x), sd = sd(x))
 
-  if(!is.null(labels)) names(output) <- labels
+  stats <- c(n = sum(!is.na(x)), n_missing = sum(is.na(x)), mean = mean(x, ...), sd = sd(x, ...), min = min(x, ...), max = max(x, ...))
+
+  if(!is.null(labels)) names(stats) <- labels
+
+  if(length(digits) == 1){
+
+    output <- round(stats, digits = digits)
+
+  } else{
+
+
+    output <- sapply(digits, function(x) round(stats, digits = x))
+
+  }
+
 
   output
-
 
 }
 
 
+compute_cat <- function(x, prop = F, na.rm = T, digits = 3){
+
+  if(prop){
+
+    output <- mean(x, na.rm = na.rm)
+  } else{
+
+    output <- sum(x, na.rm = na.rm)
+  }
+
+  round(output, digits)
+
+}
+
 #' Basic Table, Mean (SD) format.
 #'
-#' \code{compute_stats_meansdcombined} computes the basic summary statistics
-#' (N, mean, and SD) and combines mean and SD in the format 'Mean (SD)'.
+#' \code{compute_meansd} computes N, Mean and SD.
 #' @param x a numeric vector.
-#' @param labels a vector of character strings.
+#' @param combine a logical value. If \code{TRUE}, then the mean and SD are
+#' combined as 'Mean (SD)'. If \code{FALSE}, then the mean and SD are reported
+#' separately.
 #' @param digits an integer to pass to \code{round}.
+#' @param ... additional arguments to pass to \code{mean} and \code{sd}.
 #' @export
 #' @return a character vector containing the computed statistics.
-compute_stats_meansdcombined <- function(x, labels = NULL, digits = 2){
+compute_meansd <- function(x, combine = T, digits = 2){
 
 
- mean_val <- round(mean(x, na.rm = T), digits = 2)
- sd_val <- round(sd(x, na.rm = T), digits = 2)
+ mean_val <- round(mean(x, na.rm = T, ...), digits = digits)
+ sd_val <- round(sd(x, na.rm = T, ...), digits = digits)
+ if(combine == T){
 
- meansd_combined <- paste0(mean_val, " (", sd_val, ")")
+   meansd_combined <- paste0(mean_val, " (", sd_val, ")")
 
- c(n = sum(!is.na(x)), meansd = meansd_combined)
+  c(n = sum(!is.na(x)), meansd = meansd_combined)
+ } else{
 
+
+   c(n = sum(!is.na(x)), mean = mean_val, sd = sd_val)
+
+ }
 }
 
 
@@ -55,14 +89,16 @@ compute_stats_meansdcombined <- function(x, labels = NULL, digits = 2){
 #' functions to be applied to \code{x}.
 #' @param func_args a list with number of elements equal to \code{length(func_names)}.
 #' Each element of \code{func_args} should itself be a named list of arguments to be
-#' passed to the corresponding element of \code{func_names}.
+#' passed to the corresponding element of \code{func_names}. If the list consists of
+#' non-list elements, then it is assumed that this single list contains arguments
+#' that should be applied to all functions contained in \code{func_names}.
 #' @param labels a vector of character strings giving the labels for the elements
 #' of the output. Default is to use \code{func_names}.
-compute_quant <- function(x, func_names, func_args, labels = NULL){
+compute_stats <- function(x, stat_func, stat_args, labels = NULL){
 
  n_func <- length(func_names)
  output <- rep(NA, n_func)
-
+ if(!all(sapply(test_list, is.list))) func_args <- c(lapply(seq_along(func_names), function(x) func_args))
  for(i in seq_along(func_names)){
 
   output[i] <- do.call(what = func_names[i], args = func_args[[i]])
