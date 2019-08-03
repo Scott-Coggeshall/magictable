fd3ffb571b870487aa9b4f5418705295fb09ee50
SCOTT COGGESHALL <scott.coggeshall@va.gov>
2019-08-02 17:01:32 -0700

    correct lapply for variable number of digits per stat

--- R/compute_quant.R
+++ R/compute_quant.R
@@ -24,7 +24,7 @@ compute_quant <- function(x, digits = 3, labels = NULL, ...){
   } else{
 
 
-    output <- sapply(digits, function(x) round(stats, digits = x))
+    output <- sapply(seq_along(digits), function(x) round(stats[x], digits = digits[x]))
 
   }
 
